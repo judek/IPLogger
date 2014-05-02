8a7462557efd4a611a2cb9d4125c7746cd5e8ba8
jude <judek15@gmail.com>
2014-05-02 08:32:13 -0500

    Inital Commit

---
+++ IPLogger/LogIP.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IPLogger
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            string IpAdress = Request.UserHostAddress;
+
+            if (true == string.IsNullOrEmpty(IpAdress))
+                    return;
+
+            Literal1.Text = IpAdress;
+
+            string sName = Request.QueryString["name"];
+
+
+            if (true == string.IsNullOrEmpty(sName))
+                 return;
+
+
+            System.IO.File.WriteAllText(sName + ".txt", IpAdress);
+
+
+            
+
+        }
+    }
+}
